6f643cf8c363cfc6297961d8260f04035580d86c
JackShort <john.short.tj@gmail.com>
2020-11-30 23:03:19 -0500

    updated

--- config.h
+++ config.h
@@ -1,21 +1,38 @@
-/* See LICENSE file for copyright and license details. */
+/*                          __ _         _     
+*           ___ ___  _ __  / _(_) __ _  | |__  
+*          / __/ _ \| '_ \| |_| |/ _` | | '_ \
+*         | (_| (_) | | | |  _| | (_| |_| | | |
+*          \___\___/|_| |_|_| |_|\__, (_)_| |_|
+*                                |___/         
+*	dwm's configuration file (yes, config file)
+*/
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+
 static const char *fonts[]          = { "Fira Code:size=11" };
 static const char dmenufont[]       = "Fira Code:size=11";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char back[]   = "#1C1E27";
+static const char fore[]   = "#CACACC";
+static const char border[] = "#CACACC";
+static const char col0[]   = "#6C6F93";
+static const char col1[]   = "#E4436F";
+static const char col2[]   = "#24E39D";
+static const char col3[]   = "#EDA685";
+static const char col4[]   = "#00A5AF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm]     = { fore,      back,      back   }, // \x0b
+	[SchemeSel]      = { fore,      back,      col4   }, // \x0c
 };
 
 /* tagging */
