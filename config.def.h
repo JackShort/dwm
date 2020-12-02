c896e160316db24efc416f6b7ca32be88d123de8
JackShort <john.short.tj@gmail.com>
2020-12-01 19:27:35 -0500

    update

--- config.def.h
+++ config.def.h
@@ -8,7 +8,8 @@
 */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 2;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -93,7 +94,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", back, "-nf", col4, "-sb", col1, "-sf", back, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *volumeup[]  = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
 static const char *volumedown[]  = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
@@ -101,6 +102,8 @@ static const char *mutevolume[]  = { "pactl", "set-sink-mute", "@DEFAULT_SINK@",
 static const char *pausetrack[]  = { "playerctl", "play-pause", NULL };
 static const char *nexttrack[]  = { "playerctl", "next", NULL };
 static const char *prevtrack[]  = { "playerctl", "previous", NULL };
+static const char *openchrome[]  = { "chromium", NULL };
+static const char *openspotify[]  = { "spotify", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -118,15 +121,18 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,			XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[7]} }, /* float */
+	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[7]} }, /* float */
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} }, /* monacle */
 	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[2]} }, /* centeredmaster */
 	/*{ MODKEY|ShiftMask,             XK_i,      setlayout,      {.v = &layouts[3]} }, centeredmaster floating*/
 	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[4]} }, /* spiral */
 	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[5]} }, /* dwindle */
 	{ MODKEY,			XK_d,      setlayout,      {.v = &layouts[6]} }, /* doubledeck */
+	{ MODKEY,                       XK_s,	   togglesticky,   {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_f,      togglefullscreen, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefakefullscreen, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -149,6 +155,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_i,	    spawn,	    {.v = pausetrack } },
 	{ MODKEY,			XK_u,	    spawn,	    {.v = nexttrack } },
 	{ MODKEY|ShiftMask,             XK_u,	    spawn,	    {.v = prevtrack } },
+	{ MODKEY,			XK_w,	    spawn,	    {.v = openchrome } },
+	{ MODKEY|ShiftMask,             XK_w,	    spawn,	    {.v = openspotify } },
 };
 
 /* button definitions */
